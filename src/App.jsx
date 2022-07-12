2e9e2b773e60ee652a1e039a51ef18e4881bf82b
Sam Mitchell <samworth27@gmail.com>
2022-07-12 16:15:15 +1000

    basic user login after 4 refactors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from "react";
-import {Outlet, Link} from "react-router-dom"
+import { useState, useReducer } from "react";
+import { Outlet, Link } from "react-router-dom";
 import tw from "twin.macro";
 import "./index.css";
 
 import Sidebar from "./components/Sidebar";
 import ContentWrapper from "./components/ContentWrapper.jsx";
+import { UserContext, userReducer } from "./modules/User";
 
 
 function App() {
-  let [sidebarOpen, setSidebarOpen] = useState(true);
+  let [sidebarOpen, setSidebarOpen] = useState(false);
+  let initialUserState = {user:null, token:null};
+  const [userStore, userDispatch] = useReducer(userReducer, initialUserState)
   return (
     <div className="relative min-h-screen text-white">
-        <div className="relative flex h-screen bg-black">
+      <div className="relative flex h-screen bg-black">
+        <UserContext.Provider value={{ userStore, userDispatch }} >
           <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <ContentWrapper>
             <Outlet />
           </ContentWrapper>
-        </div>
+        </UserContext.Provider>
+      </div>
     </div>
   );
 }
--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,5 +1,48 @@
-function LoginPage(){
-  return(<div>LoginPage</div>)
+import { useState, useContext} from "react";
+import { UserContext } from "../../modules/User";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {userStore, userDispatch} = useContext(UserContext)
+  let user = userStore.user
+  let token = userStore.token
+
+  async function login(event){
+    event.preventDefault();
+    const credentials = { email: email, password: password };
+    const res = await fetch("http://localhost:4000/api/v1/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+    const data = await res.json();
+    const user = { id: data._id, name: data.name, email: data.email };
+    userDispatch({
+      type: "login",
+      data:{user:user, token: data.token}
+    })
+  };
+
+  return (
+    <div>
+      <div>Login</div>
+      <form className="bg-green-800 text-black" onSubmit={(event)=>login(event)}>
+        <label>
+          email:
+          <input type="text" value={email} onChange={(event)=>setEmail(event.target.value)} ></input>
+        </label>
+
+        <label>
+          password:
+          <input type="password" value={password} onChange={(event)=>setPassword(event.target.value)}></input>
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+      <div>{user && "user logged in"}</div>
+      <div>{token && "Has token"}</div>
+    </div>
+  );
 }
 
 export default LoginPage;
