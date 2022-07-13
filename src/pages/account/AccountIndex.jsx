88a21be1ae04969b98bffa51a0355119c0841c84
Sam Mitchell <samworth27@gmail.com>
2022-07-13 13:28:55 +1000

    redirect to login in not logged in

--- src/pages/account/AccountIndex.jsx
+++ src/pages/account/AccountIndex.jsx
@@ -1,14 +1,25 @@
-import GlowCard from '../../components/GlowCard'
+import GlowCard from "../../components/GlowCard";
+import { useContext } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { UserContext } from "/src/modules/User";
 
 function AccountIndex() {
+  const {
+    userStore: { user, token },
+    userDispatch,
+  } = useContext(UserContext);
+
+  if (!user) {
+    return <Navigate to="/auth/login" />;
+  }
   return (
-    <>
-      <GlowCard header="This is your account page"/>
-      <GlowCard header = "Change your deets" />
-      <GlowCard header = "These are only here" />
-      <GlowCard header = "To demonstrate"/>
-      <GlowCard header = "page scrolling"/>
-    </>
+    <div className="flex flex-wrap gap-24 items-start justify-center py-8">
+      <GlowCard header="This is your account page" />
+      <GlowCard header="Change your deets" />
+      <GlowCard header="These are only here" />
+      <GlowCard header="To demonstrate" />
+      <GlowCard header="page scrolling" />
+    </div>
   );
 }
 
