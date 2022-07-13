00d67b9d7efb1de8d68ac1a9ffa4dcfec0610017
Sam Mitchell <samworth27@gmail.com>
2022-07-13 13:28:27 +1000

    + logout button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,41 @@
-import { CgMenuGridR, CgShoppingCart, CgUser, CgList } from "react-icons/cg";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import {
+  CgMenuGridR,
+  CgShoppingCart,
+  CgUser,
+  CgList,
+  CgLogOut,
+} from "react-icons/cg";
+import {UserContext} from "/src/modules/User"
 
 function Sidebar({ sidebarOpen, setSidebarOpen }) {
+  const {userStore,userDispatch} = useContext(UserContext)
   function MenuItem({ name, children, path }) {
     return (
       <div className={`flex w-full`}>
-        <NavLink className={({isActive})=>isActive ? "text-green-500" : "" } to={path}>{children}</NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "text-green-500" : "")}
+          to={path}
+        >
+          {children}
+        </NavLink>
         {sidebarOpen && <span className="pl-2 my-auto">{name}</span>}
+      </div>
+    );
+  }
+
+  function MenuLogOut({ children }) {
+    return (
+      <div className={`flex w-full`}>
+        <button
+          onClick={() => {
+            userDispatch({ type:"logout"})
+          }}
+        >
+          {children}
+        </button>
+        {sidebarOpen && <span className="pl-2 my-auto">Log Out</span>}
       </div>
     );
   }
@@ -21,7 +50,11 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         >
           <CgMenuGridR size={"3rem"} />
         </button>
-        {sidebarOpen && <NavLink to="/" className="my-auto">PC Store</NavLink>}
+        {sidebarOpen && (
+          <NavLink to="/" className="my-auto">
+            PC Store
+          </NavLink>
+        )}
       </div>
     );
   }
@@ -34,15 +67,18 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
       <div className="flex-col divide-y h-full w-full">
         <SidebarToggle />
         <div className={`flex flex-wrap content-start px-2 pt-4 gap-4`}>
-          <MenuItem name="Account" path='account'>
+          <MenuItem name="Account" path="account">
             <CgUser size="2em" />
           </MenuItem>
-          <MenuItem name="Shop" path='shop'>
+          <MenuItem name="Shop" path="shop">
             <CgShoppingCart size="2em" />
           </MenuItem>
-          <MenuItem name="Orders" path='orders'>
+          <MenuItem name="Orders" path="orders">
             <CgList size="2em" />
           </MenuItem>
+          <MenuLogOut>
+            <CgLogOut size="2em" />
+          </MenuLogOut>
         </div>
       </div>
     </div>
