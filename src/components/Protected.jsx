312d62d3775fb7e5b8ea98b1a66466484b5ce0cd
Sam Mitchell <samworth27@gmail.com>
2022-07-18 12:31:16 +1000

    basic protected routes

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,3 +1,25 @@
-function Protected({ children }) {}
+import { Navigate, Outlet } from "react-router-dom";
 
-export default Protected;
+function ProtectedRoute() {}
+
+function LoggedIn({ user, children }) {
+  if (!user) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  return children ? children : <Outlet />;
+}
+
+function Admin({ user, children }) {
+  if (!user) {
+    return <Navigate to="/auth/login" replace />;
+  }
+  if (!user.admin) {
+    return <Navigate to={-1} replace />;
+  }
+  return children ? children : <Outlet />;
+}
+
+ProtectedRoute.LoggedIn = LoggedIn;
+ProtectedRoute.Admin = Admin;
+
+export default ProtectedRoute;
