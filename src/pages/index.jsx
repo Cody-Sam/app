dc55b8c667b829a031ff332c44840b24aecc3115
Sam Mitchell <samworth27@gmail.com>
2022-07-18 11:59:45 +1000

    simplified import/ exports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,9 @@
-import IndexPage from "./IndexPage";
-import Account from "./account";
-import Admin from "./admin";
-import Auth from "./auth";
-import Cart from "./cart";
-import Orders from "./orders";
-import Shop from "./shop";
-
-export default IndexPage;
-export {
-  Account,
-  Auth,
-  Admin,
-  Cart,
-  Orders,
-  Shop
-}
-
+export {default as Index} from "./IndexPage";
+export {default as Account} from "./account";
+export {default as Admin} from "./admin";
+export {default as Auth} from "./auth";
+export {default as Build} from "./build";
+export {default as Cart} from "./cart";
+export {default as Orders} from "./orders";
+export {default as Shop} from "./shop";
 
