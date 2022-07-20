5ee5c42d7da2074c98c96b38a80f57285953a993
Sam Mitchell <samworth27@gmail.com>
2022-07-20 20:45:34 +1000

    remove undeed files

--- src/pages/build/BuildIndex.jsx
+++ src/pages/build/BuildIndex.jsx
@@ -7,15 +7,6 @@ import {
 } from "modules/build";
 
 import BuilderOverview from "./BuilderOverview";
-import CasePage from "./CasePage";
-import CaseCoolingPage from "./CaseCoolingPage";
-import CPUCoolerPage from "./CPUCoolerPage";
-import CPUPage from "./CPUPage";
-import GPUPage from "./GPUPage";
-import MotherboardPage from "./MotherboardPage";
-import PSUPage from "./PSUPage";
-import RAMPage from "./RAMPage";
-import StoragePage from "./StoragePage";
 import SelectItemDefault from "./SelectItemDefault";
 
 function BuildIndex() {
