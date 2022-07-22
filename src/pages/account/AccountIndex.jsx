fb96dac64c35854241ab8661216eb1aa61071f2e
Cody Partington <codythatsme@gmail.com>
2022-07-22 22:34:58 +1000

    account page added

--- src/pages/account/AccountIndex.jsx
+++ src/pages/account/AccountIndex.jsx
@@ -1,8 +1,9 @@
 import GlowCard from "components/GlowCard";
 import { useContext, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { UserContext } from "modules/user";
 import ContentWrapper from "components/ContentWrapper";
+import { PageContainer } from "../../components/StyledComponents";
 
 function AccountIndex() {
   const {
@@ -11,16 +12,34 @@ function AccountIndex() {
   } = useContext(UserContext);
 
   return (
-    <ContentWrapper.Flex>
-      <GlowCard>
-        {/* <GlowCard.Header /> */}
-        <GlowCard.Body>
-          <h1 className="text-4xl z-20 text-center">
-            This is your account page
-          </h1>
-        </GlowCard.Body>
-      </GlowCard>
-    </ContentWrapper.Flex>
+    <PageContainer>
+      <ContentWrapper.Flex>
+        <Link to="../../shop">
+          <GlowCard>
+            {/* <GlowCard.Header /> */}
+            <GlowCard.Body>
+              <h1 className="text-4xl z-20 text-center">Start Shopping</h1>
+            </GlowCard.Body>
+          </GlowCard>
+        </Link>
+        <Link to="../../build">
+          <GlowCard>
+            {/* <GlowCard.Header /> */}
+            <GlowCard.Body>
+              <h1 className="text-4xl z-20 text-center">Build Your PC</h1>
+            </GlowCard.Body>
+          </GlowCard>
+        </Link>
+        <Link to="../../orders">
+          <GlowCard>
+            {/* <GlowCard.Header /> */}
+            <GlowCard.Body>
+              <h1 className="text-4xl z-20 text-center">View Your Orders</h1>
+            </GlowCard.Body>
+          </GlowCard>
+        </Link>
+      </ContentWrapper.Flex>
+    </PageContainer>
   );
 }
 export default AccountIndex;
